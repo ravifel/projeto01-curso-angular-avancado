d22e9958b467d3a3b3d9d95c4cdf7829424d024f
Ravi Silva <ravifelipee@gmail.com>
2022-07-28 02:28:59 -0300

    Habilitando o Route Tracing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,7 +21,7 @@ const rootRouterConfig: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(rootRouterConfig)
+        RouterModule.forRoot(rootRouterConfig, { enableTracing: false })
     ],
     exports: [
         RouterModule
