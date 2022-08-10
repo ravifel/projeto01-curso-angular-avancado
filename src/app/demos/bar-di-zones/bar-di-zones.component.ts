2b4507920a471b00e5463586d8406777e76eef07
Ravi Silva <ravifelipee@gmail.com>
2022-08-10 09:13:26 -0300

    Aula: Providers - UseFactory

--- src/app/demos/bar-di-zones/bar-di-zones.component.ts
+++ src/app/demos/bar-di-zones/bar-di-zones.component.ts
@@ -1,13 +1,22 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
-import { BarService, BarServiceMock } from './bar.service';
+import { BarModule } from './bar.module';
+import { BarFactory, BarService, BarServiceMock } from './bar.service';
 
 @Component({
   selector: 'app-bar-di-zones',
   templateUrl: './bar-di-zones.component.html',
   styles: [],
   providers: [
-    { provide: BarService, useClass: BarService }
+    { provide: BarService, useClass: BarService },
+    {
+      provide: BarService, useFactory: BarFactory,
+      deps: [
+        HttpClient,
+        Injector
+      ]
+    }
   ]
 })
 export class BarDiZonesComponent implements OnInit {
@@ -15,6 +24,7 @@ export class BarDiZonesComponent implements OnInit {
   ConfigManual: BarUnidadeConfig;
   Config: BarUnidadeConfig;
   barBebida1: string;
+  dadosUnidade: string;
 
   constructor(
     private BarService: BarService,
@@ -26,6 +36,8 @@ export class BarDiZonesComponent implements OnInit {
     this.barBebida1 = this.BarService.obterBebidas();
     this.ConfigManual = this.ApiConfigManual;
     this.Config = this.ApiConfig;
+
+    this.dadosUnidade = this.BarService.obterUnidade();
   }
 
 }
--- src/app/demos/bar-di-zones/bar.service.ts
+++ src/app/demos/bar-di-zones/bar.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, Injector } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from "./bar.config";
 
+export function BarFactory(http: HttpClient, Injector: Injector) {
+    return new BarService(http, Injector.get(BAR_UNIDADE_CONFIG));
+}
 
 @Injectable()
 export class BarService {
 
-    constructor(private http: HttpClient) { }
+    constructor(
+        private http: HttpClient,
+        @Inject(BAR_UNIDADE_CONFIG) private config: BarUnidadeConfig
+    ) { }
+
+    public obterUnidade(): string {
+        return 'Unidade ID: ' + this.config.unidadeId + ' Token: ' + this.config.unidadeToken
+    }
 
     obterBebidas(): string {
         return 'Bebidas';
