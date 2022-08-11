9d0009781177623aeb8e6ddcdb2db7786bab1fd8
Ravi Silva <ravifelipee@gmail.com>
2022-08-11 12:03:15 -0300

    Aula 'NgZones' do Modulo 'DI e Zonas'

--- src/app/demos/bar-di-zones/bar-di-zones.component.ts
+++ src/app/demos/bar-di-zones/bar-di-zones.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, NgZone, OnInit } from '@angular/core';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
 import { BarModule } from './bar.module';
 import { BarFactory, BarService, BarServiceMock, BebidaServiceAbstract } from './bar.service';
@@ -32,7 +32,8 @@ export class BarDiZonesComponent implements OnInit {
     private BarService: BarService,
     @Inject('ConfigManualUnidade') private ApiConfigManual: BarUnidadeConfig,
     @Inject(BAR_UNIDADE_CONFIG) private ApiConfig: BarUnidadeConfig,
-    private BebidaServiceAbstract: BebidaServiceAbstract
+    private BebidaServiceAbstract: BebidaServiceAbstract,
+    private ngZone: NgZone
   ) { }
 
   ngOnInit() {
@@ -43,6 +44,36 @@ export class BarDiZonesComponent implements OnInit {
     this.dadosUnidade = this.BarService.obterUnidade();
 
     this.barBebida2 = this.BebidaServiceAbstract.obterBebidas();
+  }
+
+  public progress: number = 0;
+  public label: string;
+
+  processWithinAngularZone() {
+    this.label = 'dentro';
+    this.progress = 0;
+    this._increaseProgress(() => console.log('Finalizado por dentro do Angular!'))
+  }
+
+  processOutsideOfAngularZone() {
+    this.label = 'fora';
+    this.progress = 0;
+    this.ngZone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        this.ngZone.run(() => { console.log('Finalizado fora!'); });
+      });
+    });
+  }
+
+  _increaseProgress(doneCallback: () => void) {
+    this.progress += 1;
+    console.log(`Progresso atual: ${this.progress}%`);
+
+    if (this.progress < 100) {
+      window.setTimeout(() => this._increaseProgress(doneCallback), 10);
+    } else {
+      doneCallback();
+    }
   }
 
 }
